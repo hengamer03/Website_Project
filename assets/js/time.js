89c06f8d4d5c762f68beca874359a89dac4dfdbc
hengamer <henning.hovden@gmail.com>
2026-01-28 10:36:15 +0100

    remove old files

--- assets/js/time.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const dateElement = document.getElementById('date');
-    if (dateElement) {
-        const d = new Date();
-        dateElement.innerHTML = `Today's date is ${d.toDateString()}`;
-    } else {
-        console.error("Date element not found in the document.");
-    }
-});
